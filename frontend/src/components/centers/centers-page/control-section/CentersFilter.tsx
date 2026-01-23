c8f874acaa8aecf5f8ad1562bfe4be88aa21190e
Abhish7k <abhishekbhosale.dev@gmail.com>
2026-01-23 22:03:50 +0530

    style: centers page card
    added hover effects on the center card component on the centers page

--- frontend/src/components/centers/centers-page/control-section/CentersFilter.tsx
+++ frontend/src/components/centers/centers-page/control-section/CentersFilter.tsx
@@ -6,7 +6,7 @@ import {
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { SlidersHorizontal, X, XIcon } from "lucide-react";
+import { SlidersHorizontal, XIcon } from "lucide-react";
 
 type Props = {
   vaccines: string[];
