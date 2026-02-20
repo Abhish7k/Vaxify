06c3fc372d98921b74f1a835eb14899aa7b068a5
Abhishek <abhishekbhosale.dev@gmail.com>
2026-02-20 13:27:35 +0530

    refactor: api
    moved the api base url to the same domain using vercel rewrites

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // a central axios instance to use for all api calls with a base config and auth headers
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
 });
 
 // intercept every req and attach jwt token to it
